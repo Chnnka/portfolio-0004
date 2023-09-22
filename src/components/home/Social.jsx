eaf4f4b49757bddbe5780491ead1d727f317bc31
Channaka Gunawardhane <channakagunawardana@gmail.com>
2023-09-22 20:09:26 +0530

    20230922-20-09

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/ch_paintings/" className="home__social-icon" target='_blank'>
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://www.behance.net/channaka" className="home__social-icon" target='_blank'>
+        <i class="uil uil-behance-alt"></i>
+      </a>
+
+      <a href="https://github.com/Chnnka" className="home__social-icon" target='_blank'>
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social;
